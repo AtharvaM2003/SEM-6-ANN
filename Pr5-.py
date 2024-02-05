84eb1147f03a6fad4000500a688fcf67a0af4ab1
AtharvaM2003 <atharva2mundhe@gmail.com>
2024-02-05 12:44:03 +0530

    ff

---
+++ Pr5-.py
@@ -0,0 +1,15 @@
+# Write a python Program for Bidirectional Associative Memory with two pairs of vectors.
+import numpy as np
+X = np.array([[1, 1, 1, -1], [-1, -1, 1, 1]])
+Y = np.array([[1, -1], [-1, 1]])
+W = np.dot(Y.T, X)
+
+def bam(x):
+    return np.sign(np.dot(W, x))
+
+x_test = np.array([1, -1, -1, -1])
+y_test = bam(x_test)
+
+
+print("Input x:", x_test)
+print("Output y:", y_test)
